49673a302ebb9d2a6a50711327545a10b63a646e
EliHaciyev <123108540+EliHaciyev@users.noreply.github.com>
2023-12-18 16:13:03 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,19 @@
-/* eslint-disable no-undef */
+
 
 const numberOfFilms = +prompt('How many movies have you been?', '')
-
-// eslint-disable-next-line no-unused-vars
 const personalMovieDB = {
 	count: numberOfFilms,
-	// eslint-disable-next-line indent
-  movies: {},
+        movies: {},
 	actors: {},
 	genres: [],
 	privat: false
 }
-
-// eslint-disable-next-line no-unused-vars
 const a = prompt('One of the movies you saw?', ''),
 	b = prompt('The last movie you saw?'),
 	c = prompt('One of the movies you saw?', ''),
 	d = prompt('The last movie you saw?')
 personalMovieD.movies[a]= b
 personalMovieD.movies[c]= d
-
 console.log(personalMovieD)
 
 
