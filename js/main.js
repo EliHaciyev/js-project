34acda364905dbcbd697ecac32decb3e609ba80e
Haciyev Ali <heli25851@gmail.com>
2023-12-18 19:52:58 +0400

    transformed the code and added new elements

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
 
-
 const numberOfFilms = +prompt('How many movies have you been?', '')
 const personalMovieDB = {
 	count: numberOfFilms,
-        movies: {},
+	movies: {},
 	actors: {},
 	genres: [],
 	privat: false
 }
-const a = prompt('One of the movies you saw?', ''),
-	b = prompt('The last movie you saw?'),
-	c = prompt('One of the movies you saw?', ''),
-	d = prompt('The last movie you saw?')
-personalMovieD.movies[a]= b
-personalMovieD.movies[c]= d
-console.log(personalMovieD)
+
+for(let i = 0; i < 2; i++) {
+	const a = prompt('One of the movies you saw?', ''),
+		b = prompt('The last movie you saw?')
+
+	if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a]= b
+		console.log('done')
+	} else {
+		console.log('error')
+		i--
+	}
+}
+
+if (personalMovieDB.count < 10) {
+	console.log('Watched a fairly small number of movies')
+}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('You a classic spectator')
+} else if (personalMovieDB.count >= 30) {
+	console.log('You a movie buff')
+} else {
+	console.log('There was an error')
+}
+
+console.log(personalMovieDB)
+
+
+
 
 
